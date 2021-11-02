e82fc7eb07a684bb0bff48a1b53466ef20299cd1
JoonsubHwang <j97194719@gmail.com>
2021-11-02 14:34:32 -0400

    Created /character/:charName route and its functions

--- index.js
+++ index.js
@@ -22,6 +22,62 @@ app.use(cors);
 
 
 
+// route
+
+app.get('/character/:charName', (req, res) => {
+    loadCharData(req.params.charName)
+        .then(charData => {
+            res.send(charData);
+        })
+        .catch(err => {
+            res.status(status.serverError).send(err);
+        })
+});
+
+
+
+// helpers
+
+async function loadHTML(url) {
+    try {
+        let res = await axios(url);
+        return cheerio.load(res.data);
+    } catch (err) {
+
+        console.error('[loadHTML] Error: ' + err);
+        throw err;
+    }
+}
+
+async function loadCharData(charName) {
+
+    const url = `https://m-lostark.game.onstove.com/Profile/Character/${charName}`;
+
+    try {
+
+        const $ = await loadHTML(url);
+        
+        // TODO: check no character 
+        // <script> alert('캐릭터 정보가 없습니다. 캐릭터명을 확인해주세요.'); location.replace('/Profile/Character') </script>
+
+        // TODO: check maintenance
+
+        const charData = {
+            charLevel: $('#myinfo__character--button2').text().split(' ')[0],
+            charName: $('#myinfo__character--button2').text().split(' ')[1]
+        }
+
+        return charData;
+
+    } catch (err) {
+
+        console.error('[loadCharData] Error: ' + err);
+        throw new Error('unknown', 'Failed to load character data.');
+    }
+}
+
+
+
 // start server
 
 app.listen(HTTP_PORT, () => {
